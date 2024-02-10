75ee1d36c935a7a8757d8748cdc1c704d6d71f0f
moonrailgun <moonrailgun@gmail.com>
2024-02-10 19:35:10 +0800

    chore: init

---
+++ packages/react/src/index.ts
@@ -0,0 +1,97 @@
+import i18next, { TFunction, TOptionsBase } from 'i18next';
+import {
+  useTranslation as useI18NTranslation,
+  initReactI18next,
+} from 'react-i18next';
+import { crc32 } from 'crc';
+import { defaultLanguage, languageDetector } from './language';
+import { useState, useEffect } from 'react';
+import HttpApi from 'i18next-http-backend'; // https://github.com/i18next/i18next-http-backend
+
+i18next
+  .use(languageDetector)
+  .use(HttpApi)
+  .use(initReactI18next)
+  .init({
+    fallbackLng: defaultLanguage,
+    load: 'currentOnly',
+    backend: {
+      loadPath: '/locales/{{lng}}/{{ns}}.json',
+      allowMultiLoading: false,
+      addPath: (...args: any[]) => {
+        console.log('Lost translate:', ...args);
+      },
+    },
+    react: {
+      // Reference: https://react.i18next.com/latest/trans-component#i-18-next-options
+      hashTransKey(defaultValue: string) {
+        // return a key based on defaultValue or if you prefer to just remind you should set a key return false and throw an error
+        return `k${crc32(defaultValue).toString(16)}`;
+      },
+    },
+  } as any);
+
+export { i18next };
+
+/**
+ * standard t function
+ * use for global but not cool for react
+ */
+export const t = (
+  key: string,
+  defaultValue?: string,
+  options?: TOptionsBase & Record<string, unknown>
+) => {
+  try {
+    const hashKey = `k${crc32(key).toString(16)}`;
+    let words = i18next.t(hashKey, defaultValue ?? '', options);
+    if (words === hashKey) {
+      words = key;
+      console.info(`[i18n] Lost translation: [${hashKey}]${key}`);
+    }
+    return words;
+  } catch (err) {
+    console.error(err);
+    return key;
+  }
+};
+
+/**
+ * local translation without file
+ *
+ * @example
+ * localTrans({'zh-CN': '你好', 'en-US': 'Hello'});
+ */
+export function localTrans(trans: Record<string, string>) {
+  const lang = i18next.language;
+  return trans[lang] ?? trans[defaultLanguage];
+}
+
+/**
+ * set language
+ */
+export async function setLanguage(lang: string): Promise<void> {
+  return new Promise((resolve, reject) => {
+    i18next.changeLanguage(lang, (err) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve();
+      }
+    });
+  });
+}
+
+/**
+ * i18n for react
+ */
+export function useTranslation() {
+  const { t: i18nT, ready } = useI18NTranslation();
+
+  const [_t, _setT] = useState<TFunction>(() => t as any);
+  useEffect(() => {
+    _setT(() => ((...args: any[]) => (t as any)(...args)) as any);
+  }, [i18nT]);
+
+  return { t: _t, ready };
+}
---
+++ packages/scanner/src/index.ts
@@ -0,0 +1,134 @@
+import vfs from 'vinyl-fs';
+import sort from 'gulp-sort';
+// @ts-ignore
+import scanner from 'i18next-scanner';
+import fs from 'fs-extra';
+import { resolve, extname } from 'path';
+import without from 'lodash.without';
+import { crc32 } from 'crc';
+import esbuild from 'esbuild';
+
+interface BuildConfig {
+  input: string[];
+  output: string;
+  lngs: string[];
+  options?: any;
+  transform?: (
+    this: any,
+    file: any,
+    enc: BufferEncoding,
+    done: () => void
+  ) => void;
+}
+
+const defaultConfigOptions = {
+  debug: false,
+  sort: true,
+  func: false,
+  trans: false,
+  defaultLng: 'en',
+  resource: {
+    loadPath: './{{lng}}/{{ns}}.json',
+    savePath: './{{lng}}/{{ns}}.json',
+    jsonIndent: 2,
+    lineEnding: '\n',
+    endWithEmptyTrans: true,
+  },
+  removeUnusedKeys: true,
+  nsSeparator: false, // namespace separator
+  keySeparator: false, // key separator
+  interpolation: {
+    prefix: '{{',
+    suffix: '}}',
+  },
+};
+
+export async function buildTranslationFile(config: BuildConfig) {
+  const defaultTranslationPath = './en/translation.json';
+  const originJson = fs.readJsonSync(
+    resolve(config.output, defaultTranslationPath)
+  );
+
+  const options = {
+    ...defaultConfigOptions,
+    lngs: config.lngs,
+    ...config.options,
+  };
+
+  options.resource = {
+    ...options.resource,
+    loadPath: resolve(config.output, './{{lng}}/{{ns}}.json'),
+    savePath: resolve(config.output, './{{lng}}/{{ns}}.json'),
+  };
+
+  const mainstream = vfs
+    .src([...config.input])
+    .pipe(sort()) // Sort files in stream by path
+    .pipe(scanner(options, config.transform))
+    .pipe(vfs.dest('./'));
+
+  mainstream.on('finish', () => {
+    const transJson = fs.readJsonSync(
+      resolve(config.output, defaultTranslationPath)
+    );
+    const originKeys = Object.keys(originJson);
+    const transKeys = Object.keys(transJson);
+    const addedNum = without(transKeys, ...originKeys).length;
+    const deletededNum = without(originKeys, ...transKeys).length;
+
+    console.log(
+      `The project translation has been generated! Added ${addedNum} translations, Removed ${deletededNum} translations`
+    );
+  });
+}
+
+export async function defaultTransform(
+  this: any,
+  file: any,
+  enc: BufferEncoding,
+  done: () => void
+) {
+  // Use this function to process key or value yourself
+  'use strict';
+  const parser = this.parser;
+  const content = await fs.readFile(file.path, enc);
+
+  parser.parseFuncFromString(
+    content,
+    { list: ['lang', 't'] },
+    (key: string, options: any) => {
+      options.defaultValue = key;
+      const hashKey = `k${crc32(key).toString(16)}`;
+      parser.set(hashKey, options);
+    }
+  );
+
+  // If it is a tsx file, use esbuild to convert it to jsx and then input it.
+  if (extname(file.path) === '.tsx') {
+    const { code } = await esbuild.transform(content, {
+      jsx: 'preserve',
+      loader: 'tsx',
+    });
+    parser.parseTransFromString(
+      code,
+      { component: 'Trans', i18nKey: 'i18nKey' },
+      (key: string, options: any) => {
+        /**
+         * Deal with problems caused by inconsistency between scanner and react-i18next algorithms
+         * Reference: https://github.com/i18next/i18next-scanner/issues/125
+         */
+        let sentence = options.defaultValue;
+        // remove <Tag> surrounding interopations to match i18next simpilied result
+        // @see https://github.com/i18next/react-i18next/blob/master/CHANGELOG.md#800
+        sentence = sentence.replace(/<(\d+)>{{(\w+)}}<\/\1>/g, '{{$2}}');
+        sentence = sentence.replace(/\s+/g, ' ');
+        options.defaultValue = sentence;
+
+        const hashKey = `k${crc32(key || sentence).toString(16)}`;
+        parser.set(hashKey, options);
+      }
+    );
+  }
+
+  done();
+}
