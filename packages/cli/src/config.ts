be10159f81d72eff203b11f1d531d891c2bfd091
moonrailgun <moonrailgun@gmail.com>
2024-07-30 00:52:26 +0800

    refactor: update config of cli

--- packages/cli/src/config.ts
+++ packages/cli/src/config.ts
@@ -58,21 +58,7 @@ export const configSchema = z.object({
             .optional(),
           apiKey: z.string().optional(),
           modelName: z
-            .enum([
-              'gpt-3.5-turbo',
-              'gpt-3.5-turbo-0125',
-              'gpt-3.5-turbo-1106',
-              'gpt-3.5-turbo-instruct',
-              'gpt-4',
-              'gpt-4-0613',
-              'gpt-4-32k',
-              'gpt-4-32k-0613',
-              'gpt-4-1106-vision-preview',
-              'gpt-4-vision-preview',
-              'gpt-4-1106-preview',
-              'gpt-4-turbo-preview',
-              'gpt-4-0125-preview',
-            ])
+            .string()
             .describe(
               'Check out this url: https://platform.openai.com/docs/models/overview'
             )
